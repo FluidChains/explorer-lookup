9d12c1034d310f5ce35dfe4c4d24f4b9d6d48eb7
Karim Boucher <boucherkarim@gmail.com>
2021-09-30 04:37:49 -0400

    Update `.eslintrc`

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,9 +15,12 @@ module.exports = {
   },
   rules: {
     'semi': 'off',
+    "comma-dangle": "off",
+    "@typescript-eslint/comma-dangle": ["error"],
     '@typescript-eslint/semi': ['error', 'always'],
     '@typescript-eslint/no-empty-function': ['error', { 'allow': ['arrowFunctions'] }],
-    '@typescript-eslint/member-delimiter-style': ['error', { 'multiline': { 'delimiter': 'semi', 'requireLast': true }}],
+    '@typescript-eslint/space-before-function-paren': ['error', 'always'],
+    '@typescript-eslint/member-delimiter-style': ['error', { 'multiline': { 'delimiter': 'semi', 'requireLast': true } }],
     '@typescript-eslint/interface-name-prefix': [0],
     '@typescript-eslint/strict-boolean-expressions': [0],
     '@typescript-eslint/no-explicit-any': [0], // remove one day, maybe. This is early days so we have some anys.
